6b8ca378d7544da40129cd911d27cbf3e221a089
ticccco <23436953+LucasPlacentino@users.noreply.github.com>
2023-12-11 21:44:15 +0100

    Create initial main_flask.py

---
+++ main_flask.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+  
+app = Flask(__name__) 
+  
+  
+@app.route("/") 
+def home(): 
+    return render_template("home.html", user=user) 
+  
+  
+@app.route("/discord-login") 
+def discord_login(): 
+    if user.site_lang = "en":
+        return render_template("discord-login_en.html"; user=user)
+    else:
+        return render_template("discord-login_fr.html", user=user) 
+  
+  
+@app.route("/user") 
+def user(): 
+    if user.site_lang = "en":
+        return render_template("user_en.html", user=user)
+    else:
+        return render_template("user_fr.html", user=user)
+  
+@app.route("/test") 
+def test():
+    test_user = User(
+        "Test",
+        "test@example.com"
+    )
+    return render_template("test.html", user=test_user)
+  
+  
+@app.route("/bot") 
+def bot(): 
+    # TODO: check if bot status is online
+    bot_status = "Online"
+    return render_template("bot.html", status=bot_status) 
+  
+  
+if __name__ == "__main__":
+    # TODO: run Disnake and Flask in different threads ? asyncio ?
+    app.run(debug=False)
+    
+
